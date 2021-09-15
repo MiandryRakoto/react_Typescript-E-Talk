43186416e8df4dbf0c8f80844550cb08aa3de044
Miandry05 <86937367+MiandryRakoto@users.noreply.github.com>
2021-09-16 00:24:49 +0300

    ifandray/tweekly

---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import '../App'; 
+import './styles.css';
+
+const NavBar = () => {
+    return (
+      <section>
+          <div className="container">
+            <header>
+                <a href="#" className="titre"> E-TALK </a>
+                <div className="search">
+                    <i className="fas fa-search"></i>
+                    <input type="text" placeholder="  Rechercher..." />
+                </div>    
+                <ul>
+                    <li>   
+                        <a href="#"><i className="far fa-user-circle"></i>Miandry05</a>
+                    </li>
+                    <li>
+                        <a href="#"><i className="far fa-bell"></i>Notifications</a>
+                    </li>
+                </ul>
+                <button>Se déconnecter</button>
+            </header>
+            <footer>
+                <h2>RAKOTOARIVELO Miandry IMTICIA3 N12</h2>
+            </footer>
+          </div>
+      </section>
+    )
+}
+
+export default NavBar
